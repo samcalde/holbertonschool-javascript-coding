bd290682a7d269ec58cf23022ef76236592dd716
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-25 11:51:10 -0500

    task 0 test 1

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  console.error('No file provided.');
+  process.exit(1);
+}
+
+const path = process.argv[2];
+
+fs.readFile(path, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data)
+});
